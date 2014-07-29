923bd2ac37eee2bddba56f1d74c3feb3c6b7fc75
mozesmarcell <marcell.mozes@gmail.com>
2014-07-29 15:45:54 +0200

    Első négy oldal

--- app/assets/i18n-hu.js
+++ app/assets/i18n-hu.js
@@ -19,5 +19,40 @@ Ember.I18n.translations = {
     '2-box-szoveg': 'Lorem Ipsum',
     '3-box-cim': 'Hogy vehetek részt?',
     '3-box-szoveg': 'Lorem Ipsum'
+  },
+
+  'szobak': {
+    'cim': 'Szobáink',
+    'elso-bekezdes': 'A BezárLakban két teljesen különböző kalanddal várunk Titeket, melyek egy-egy egyedileg berendezett, adott téma köré épülő, különböző kihívásokat jelentő szobát takarnak.',
+    'masodik-bekezdes': 'A szobák megoldásához semelyik esetben <strong>nincs szükség különleges ismeretekre,</strong> csupán figyelemre, jó csapatmunkára és nem utolsó sorban logikára. A fő eltérés a témákban, hangulatban mutatkozik meg, de bátran ajánljuk, hogy mindkét szobát próbáljátok ki érdeklődési körtől függetlenül.',
+    'vegas-box-cim': 'Vegas',
+    'vegas-box-elso-sor': '<strong>Téma:</strong> kaszinó, póker',
+    'vegas-box-masodik-sor': '2-5 játékos',
+    'vegas-box-harmadik-sor': '60 perc',
+    'vegas-box-gomb': 'Részletek',
+    'science-box-cim': 'Science',
+    'science-box-elso-sor': '<strong>Téma:</strong> tudomány',
+    'science-box-masodik-sor': '2-4 játékos',
+    'science-box-harmadik-sor': '60 perc',
+    'science-box-gomb': 'Részletek'
+  },
+
+  'felugro': {
+    'vegas-cim': 'Vegas',
+    'science-cim': 'Science'
+  },
+
+  'arak': {
+    'cim': 'Árak',
+    'szoveg': 'Játékaink',
+    'listaar-cim': 'Listaár',
+    'listaar-terem': 'Melyik terem érdekel?',
+    'listaar-letszam': 'Hányan vagytok?',
+    'listaar-diak': 'Van legalább kettőtöknek érvényes diákigazolványa?',
+    'listaar-voucher': 'Rendelkeztek voucherrel?',
+    'listaar-fejenkent': 'Fejenként: ',
+    'foglalas': 'Foglalas',
+    'aktualis-cim': 'Aktuális akciónk',
+    'aktualis-fb': 'Csatlakozz Facebook oldalunkhoz, hogy ne maradj le egyetlen akciónkról sem!'
   }
 };
