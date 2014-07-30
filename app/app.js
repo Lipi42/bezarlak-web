3112f7c51717538f52c02fb0d5873670d11a2782
Chris Lipcsei <krisztian.lipcsei@contentive.hu>
2014-07-30 22:02:40 +0200

    semmi

--- app/app.js
+++ app/app.js
@@ -191,3 +191,21 @@ App.LangFoglalasController = Ember.Controller.extend({
         }
     }
 });
+
+App.Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-53378724-1', 'auto');
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
