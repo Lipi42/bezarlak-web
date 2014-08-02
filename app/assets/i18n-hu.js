cc50b241e888e276c7554412caddba391d933d9c
mozesmarcell <marcell.mozes@gmail.com>
2014-08-02 17:22:45 +0200

    forditas-magyar

--- app/assets/i18n-hu.js
+++ app/assets/i18n-hu.js
@@ -10,15 +10,23 @@ Ember.I18n.translations = {
 
   'bezarlak': {
     'mi-a-bezarlak': 'Mi a BezárLak?',
-    'elso-sor': '60 perc rejtvényfejtés, csapatmunka, játékos feladatmegoldás.',
-    'masodik-sor': 'Egy cél van: kijutni, mielőtt lejár az idő.',
-    'cta': 'Nézd meg szobáinkat',
-    '1-box-cim': 'Miről szól?',
-    '1-box-szoveg': 'Lorem Ipsum',
-    '2-box-cim': 'Kinek ajánljuk?',
-    '2-box-szoveg': 'Baráti társaságoknak <br /> Cégeknek, szervezeteknek <br />Aki szereti a kihívásokat',
-    '3-box-cim': 'Hogy vehetek részt?',
-    '3-box-szoveg': 'Szervezz csapatot <br /> Válasszatok termet <br/> Foglaljatok időpontot'
+    'bal-elso-bek': 'Mások szabadulós játéknak hívják; ez persze valakinek, aki még sosem próbálta ki, nem mond sokat.',
+    'bal-masodik-bek': 'Egy szóval: csapatjáték.',
+    'bal-harmadik-bek': 'A legtöbb ember kedveli az olyan kihívásokat, amelyek szórakoztatóak, izgalmasak és teljesíthetőek. Másoktól elszigetelten, néhány választott társsal, megbirkózni egy új környezet kihívásaival.',
+    'bal-negyedik-bek': 'Felfedezni, haladni, megoldani. Sikerélményeket gyűjteni, melyekért valóban meg kell küzdeni.',
+    'bal-otodik-bek': 'rről szól a BezárLak.',
+    'bal-hatodik-bek': 'Egy cél lebeg a szemetek előtt: kijutni a szobából, mielőtt lejár a számláló. Itt azonban semmi sem az, aminek látszik. Csak a csapatmunkátokon és a logikátokon múlik, hogy sikerül-e.',
+    'bal-hetedik-bek': 'És pont ettől izgalmas.',
+    'jobb-elso-bek': 'Valamit nem vettünk észre. Bejutottunk a sötét szobába, de egyelőre semmit sem találtunk.',
+    'jobb-masodik-bek': 'Ketyeg az óra.',
+    'jobb-harmadik-bek': 'Van egy kulcsunk, amiről nem tudjuk, hogy hova való. Nem jöttünk még rá a táblára és a lábnyomokra. Ezen kívül van egy pohár vizünk, amit valaki itt felejthetett.',
+    'jobb-negyedik-bek': 'A többiek a táblát próbálják megfejteni, én pedig a sötétben matatok. Nagyon sok lakat van még hátra, és egy csomó szimbólum. Ó, egy tölcsér! Várjunk csak...',
+    'jobb-otodik-bek': 'egy BezárLak játékos naplója',
+    'also-cim': 'Mindössze három dolgot kell tennetek, hogy Ti is átélhessétek:',
+    'also-1': '<strong>Alapítsatok csapatot</strong> legalább ketten, legfeljebb öten. Pároddal, barátaiddal, családoddal, ellenségeddel: bárkivel jöhetsz, aki egy izgalmas kalandra vágyik.',
+    'also-2' : '<strong>Válasszatok szobát!</strong> Két szobánk egy-egy, témájában és nehézségében eltérő kalandot rejt. ',
+    'also-3': '<strong>Foglaljatok időpontot</strong> online vagy telefonon. Ha felkészültek vagytok, akár a foglalás napján is eljöhettek.',
+    'gomb': 'Megnézem a szobákat!'
   },
 
   'szobak': {
@@ -34,7 +42,15 @@ Ember.I18n.translations = {
     'science-box-elso-sor': '<strong>Téma:</strong> tudomány',
     'science-box-masodik-sor': '2-4 játékos',
     'science-box-harmadik-sor': '60 perc',
-    'science-box-gomb': 'Részletek'
+    'science-box-gomb': 'Részletek',
+    'vegas-felugro-elso-bek': 'Vegas pályánkon a póker világába kalauzolunk el titeket. Átélhetitek egy valódi kártyaterem hangulatát, miközben különbözö ügyességi feladatok és kihívó logikai feladványok állják utatokat és nehezítik meg a szobából való kijutásotokat.',
+    'vegas-felugro-masodik-bek': 'A feladványok megoldására és az ajtó kinyitására <strong>60 perc</strong> áll rendelkezésetekre, melyet <strong>2-5 fős</strong> csapatokban teljesíthettek.',
+    'vegas-felugro-harmadik-bek': 'Ezt a szobánkat <strong>elsősorban tapasztaltabb játékosoknak</strong> ajánljuk. Nem hátrány, ha láttatok már hasonló játékot, de természetesen ez nem követelmény.',
+    'vegas-felugro-gomb': 'Mennyivel szállhatok be?',
+    'science-felugro-elso-bek': 'Bár a név nem mindenki számára &ndash; például a főleg humán tárgyak iránt érdeklődők számára &ndash; vonzó, semmiképp se felejtsük el, hogy tudomány nélkül nem lenne számítógép, Iron Man vagy éppen <strong>látványos UV fények.</strong>',
+    'science-felugro-masodik-bek': 'A témája ellenére a szoba nem igényel semmilyen előképzettséget; ezt a kalandot egyaránt élvezheti az is, aki csak itt ismerkedik meg az élő kijutós játékok csodájával, valamit az is, aki a legnagyobb profik közé tartozik.',
+    'science-felugro-harmadik-bek': '<strong>2-4 fős</strong> csapatokban várunk titeket, a kijutásra <strong>60 percetek van.</strong> Készen álltok?',
+    'science-felugro-gomb': 'Mennyibe kerül?'
   },
 
   'felugro': {
@@ -46,11 +62,13 @@ Ember.I18n.translations = {
     'cim': 'Árak',
     'szoveg': 'Játékaink',
     'listaar-cim': 'Listaár',
+    'listaar-elso-bek': 'Kalkulátorunkkal kiszámolhatod, mennyibe kerül az általatok választott játék fejenként.',
+    'listaar-masodik-bek': 'Amennyiben ajándékba szeretnéd adni, vagy céges csapatépítés érdekel, kérlek telefonon érdeklődj!',
     'listaar-terem': 'Melyik terem érdekel?',
     'listaar-letszam': 'Hányan vagytok?',
     'listaar-diak': 'Van legalább kettőtöknek érvényes diákigazolványa?',
-    'listaar-voucher': 'Rendelkeztek voucherrel?',
     'listaar-fejenkent': 'Fejenként: ',
+    'listaar-osszesen': 'összesen: ',
     'foglalas': 'Foglalás',
     'aktualis-cim': 'Aktuális akciónk',
     'aktualis-fb': 'Csatlakozz Facebook oldalunkhoz, hogy ne maradj le egyetlen akciónkról sem!'
@@ -72,7 +90,9 @@ Ember.I18n.translations = {
     'kerdes-cim': 'Kérdésed van?',
     'kerdes-szoveg': 'Ha akad bármi, ami még érdekel, az alábbi módokon érhetsz el minket:',
     'kerdes-telefon': 'Telefonon: ',
-    'kerdes-email': 'e-mailben: ', 
+    'kerdes-email': 'BezárLak email', 
+    'kerdes-twitter': 'BezárLak a Twitteren', 
+    'kerdes-facebook': 'BezárLak a Facebookon', 
     'erkezes-cim': 'Hogyan érkeztek?',
     'erkezes-elso-bekezdes': 'A BezárLak szobái az alul látható címen találhatóak. Attól függően, hogy hogy szeretnél érkezni, két lehetőséget ajánlunk.',
     'erkezes-masodik-bekezdes': 'Tömegközlekedéssel:',
